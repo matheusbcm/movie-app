fe1c0ddf0ca5dd059731b6a8de0dbb8358fbd895
matheusbcm <matheus.2013.carvalho@hotmail.com>
2025-01-27 16:38:20 -0300

    bug-fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
+import RootLayout from "./rootLayout/RootLayout";
+import Home from "./pages/Home.tsx";
+import Sign from "./pages/Sign.tsx";
+import Login from "./pages/Login.tsx";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="sign" element={<Sign />} />
+        <Route path="login" element={<Login />} />
+      </Route>
+    )
+  );
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div className="w-screen h-screen bg-white flex flex-col ">
+      <div className="max-h-4 max-w-6 flex-1 flex flex-col">
+        <header>
+          <p>easd</p>
+        </header>
+        <main className="">
+          <h1>oi</h1>
+        </main>
+        <footer>
+          <p>oi</p>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
