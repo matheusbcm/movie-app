41c01544cf6b62e4b8a5fd413001648df73b932a
matheusbcm <matheus.2013.carvalho@hotmail.com>
2025-02-19 18:39:10 -0300

    feat-comp-carousel

---
+++ src/components/CarouselComponent.tsx
@@ -0,0 +1,33 @@
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
+import { mainContext } from "../context/MainProvider";
+import { ITrendingMovies } from "../interfaces/ITrendingMovies";
+import { useContext } from "react";
+
+const CarouselComponent = () => {
+  const { trendingMovies } = useContext(mainContext) as {
+    trendingMovies: ITrendingMovies[];
+  };
+
+  return (
+    <div className="flex justify-center">
+      <Carousel pause="hover" controls={false} indicators={false}>
+        {trendingMovies.map((movie: ITrendingMovies) => (
+          <Carousel.Item key={movie.id} interval={5000} className="flex-row">
+            <img
+              className="d-block w-80 h-auto"
+              src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            {/* <Carousel.Caption>
+              <h3>{movie.title}</h3>
+              <p>{movie.overview}</p>
+            </Carousel.Caption> */}
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+export default CarouselComponent;
