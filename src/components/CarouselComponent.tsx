19628b64688f6337c362977ab132a430fd722441
matheusbcm <matheus.2013.carvalho@hotmail.com>
2025-02-19 19:05:17 -0300

    feat-comp-carousel fix images

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -3,6 +3,7 @@ import Carousel from "react-bootstrap/Carousel";
 import { mainContext } from "../context/MainProvider";
 import { ITrendingMovies } from "../interfaces/ITrendingMovies";
 import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 const CarouselComponent = () => {
   const { trendingMovies } = useContext(mainContext) as {
@@ -10,19 +11,21 @@ const CarouselComponent = () => {
   };
 
   return (
-    <div className="flex justify-center">
-      <Carousel pause="hover" controls={false} indicators={false}>
+    <div className="w-[428px] h-auto">
+      <Carousel pause="hover" controls={true} indicators={true}>
         {trendingMovies.map((movie: ITrendingMovies) => (
-          <Carousel.Item key={movie.id} interval={5000} className="flex-row">
-            <img
-              className="d-block w-80 h-auto"
-              src={`https://image.tmdb.org/t/p/w780/${movie.poster_path}`}
-              alt={movie.title}
-            />
-            {/* <Carousel.Caption>
+          <Carousel.Item key={movie.id} interval={5000} className="">
+            <Link to={`/details/${movie.id}`}>
+              <img
+                className="w-[428px] h-auto"
+                src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}
+                alt={movie.title}
+              />
+              {/* <Carousel.Caption>
               <h3>{movie.title}</h3>
               <p>{movie.overview}</p>
             </Carousel.Caption> */}
+            </Link>
           </Carousel.Item>
         ))}
       </Carousel>
