3d8f3f2abb4f01143474bbd138a5735387b6576f
matheusbcm <matheus.2013.carvalho@hotmail.com>
2025-02-19 20:36:34 -0300

    feat-comp-carousel fixing carousel

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -11,21 +11,40 @@ const CarouselComponent = () => {
   };
 
   return (
-    <div className="w-[428px] h-auto">
-      <Carousel pause="hover" controls={true} indicators={true}>
+    <div className="w-full px-2 bg-slate-600 flex justify-center">
+      <Carousel
+        pause="hover"
+        controls={true}
+        indicators={true}
+        className="
+          [&_.carousel-control-prev]:hidden
+          [&_.carousel-control-next]:hidden
+          [&_.carousel-indicators]:hidden
+          md:[&_.carousel-control-prev]:block
+          md:[&_.carousel-control-next]:block
+          md:[&_.carousel-indicators]:hidden
+          lg:[&_.carousel-control-prev]:block
+          lg:[&_.carousel-control-next]:block
+          lg:[&_.carousel-indicators]:block
+        "
+      >
         {trendingMovies.map((movie: ITrendingMovies) => (
-          <Carousel.Item key={movie.id} interval={5000} className="">
+          <Carousel.Item key={movie.id} interval={3000}>
             <Link to={`/details/${movie.id}`}>
               <img
-                className="w-[428px] h-auto"
+                className="w-full h-auto sm:h-[200px] md:h-[400px] lg:h-[600px] object-cover rounded-lg"
                 src={`https://image.tmdb.org/t/p/w780/${movie.backdrop_path}`}
                 alt={movie.title}
               />
-              {/* <Carousel.Caption>
-              <h3>{movie.title}</h3>
-              <p>{movie.overview}</p>
-            </Carousel.Caption> */}
             </Link>
+            <Carousel.Caption className="h-1/4 absolute bottom-0 left-0 right-0 p-0 bg-black bg-opacity-50 rounded-lg">
+              <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-0">
+                {movie.title}
+              </h3>
+              <p className="text-white text-sm sm:text-base md:text-lg lg:text-xl">
+                ⭐ {movie.vote_average}/10
+              </p>
+            </Carousel.Caption>
           </Carousel.Item>
         ))}
       </Carousel>
