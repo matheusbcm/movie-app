e836705af845784ff1acd379ea2b2d3fa64bfca1
Andre Cadete <andrecadete70@gmail.com>
2025-02-24 21:44:19 +0100

    setting up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,60 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
     return (  
-        <footer>
-            <h1>Footer</h1>
+        <footer className="footer text-neutral-200 fixed bottom-0 left-0 right-0 flex justify-around items-center z-10 p-4 max-w-4xl mx-auto">
+            <ul className="menu menu-horizontal bg-neutral-700 rounded-box flex-grow items-center justify-around w-100%">
+                <li className="text-center">
+                    <a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    </a>
+                </li>
+                <li className="text-center">
+                    <Link to={'/'}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                        </svg>
+                    </Link>
+                </li>
+                <li className="text-center">
+                    <a>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                    </svg>
+                    </a>
+                </li>
+            </ul>
         </footer>
     );
-}
+};
 
 export default Footer;
